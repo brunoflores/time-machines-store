16ae4cbcde6a7c81a4af146e5df208aa608158f7
Bruno Flores <bruno@brunoflores.com.br>
2021-09-14 08:24:42 +1000

    Refactor gender stats.

--- src/Lib.hs
+++ src/Lib.hs
@@ -43,14 +43,24 @@ clientName client = case client of
     case p of
       Person {firstName = f, lastName = l} -> f ++ " " ++ l
 
-data GenderCount = GenderCount Int Int Int
+data GenderStats = GenderStats Int Int Int
 
-countGenders :: [Client] -> GenderCount -> GenderCount
-countGenders [] stats = stats
-countGenders ((Individual (Person _ _ Male) _) : xs) (GenderCount m f u) = countGenders xs (GenderCount (m + 1) f u)
-countGenders ((Individual (Person _ _ Female) _) : xs) (GenderCount m f u) = countGenders xs (GenderCount m (f + 1) u)
-countGenders ((Individual (Person _ _ Unknown) _) : xs) (GenderCount m f u) = countGenders xs (GenderCount m f (u + 1))
-countGenders (_ : xs) stats = countGenders xs stats
+listGenders :: [Client] -> [Gender]
+listGenders [] = []
+listGenders ((Individual (Person _ _ Male) _) : xs) = [Male] ++ listGenders xs
+listGenders ((Individual (Person _ _ Female) _) : xs) = [Female] ++ listGenders xs
+listGenders ((Individual (Person _ _ Unknown) _) : xs) = [Unknown] ++ listGenders xs
+listGenders (_ : xs) = listGenders xs
+
+countGenders :: [Gender] -> GenderStats
+countGenders = foldr calc (GenderStats 0 0 0)
+  where
+    calc Male (GenderStats m f u) = GenderStats (m + 1) f u
+    calc Female (GenderStats m f u) = GenderStats m (f + 1) u
+    calc Unknown (GenderStats m f u) = GenderStats m f (u + 1)
+
+calcGenders :: [Client] -> GenderStats
+calcGenders xs = countGenders (listGenders xs)
 
 responsibility :: Client -> String
 responsibility (Company _ _ _ r) = r
